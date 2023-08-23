ace23c7d91656752be92ad27d8c6ad121350266d
loboxos <vcristian528@gmail.com>
2023-08-23 12:54:50 -0300

    fundamentos

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
+<!-- single file component -->
+<script setup>
+const name = "Vue dinamico";
+const styleColor = "color:blue";
+const arrayColores = ["blue","red","peru"];
+const activo = true;
+</script>
+
 <template>
-  <h1>comienzo del proyecto</h1>
-</template>
+  <h1>comienzo del proyecto {{ name.toUpperCase() }}</h1>
+  <!-- v-bind es igual a solo poner : -->
+  <h2 v-bind:style="styleColor">soy azul owo </h2>
+  <h2>{{ arrayColores }}</h2>
+  <h2 :style="`color: ${arrayColores[1]}`">soy azul owo </h2>
+  <h2>{{ activo ? 'estoy activo' : 'estoy inactivo' }} </h2>
+  <p></p>
+</template> 
+
+<style>
+h1 {
+  color:aqua;
+}
+</style>
